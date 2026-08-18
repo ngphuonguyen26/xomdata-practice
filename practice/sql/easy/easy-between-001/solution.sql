afa5779d2fd4c118d08be8bfd3e3372926e3fd27
ngphuonguyen26 <210934804+ngphuonguyen26@users.noreply.github.com>
2026-08-18 12:45:18 +0000

    solve: 2 problems (2026-08-18)
    - easy-avg-003
    - easy-between-001

---
+++ practice/sql/easy/easy-between-001/solution.sql
@@ -0,0 +1,7 @@
+-- Xom Data · Products in a price range
+-- Problem: https://xomdata.com/practice/easy-between-001
+-- Solved: 2026-08-18
+
+-- Viết SQL của bạn ở đây
+select product_name, price from products
+where price between 200 and 500;
